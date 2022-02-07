0d5673eff387305735d425686413a06d372af34f
mdbow22 <mdbow22@gmail.com>
2022-02-06 21:24:11 -0500

    set daisyUI theme colors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="hero bg-base-200 min-h-screen">
         <div>
           <h1 className="mb-5 text-5xl font-bold">WeatherBoard</h1>
-          <button className="btn btn-primary">Use my Location</button>
+          <button className="btn btn-primary rounded-none shadow shadow-gray-400">Use my Location</button>
         </div>
       </header>
     </div>
